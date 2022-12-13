6db15613407f2f441a3c3b16fa6a204d0bca81cc
nikmakpak <nicement28@gmail.com>
2022-12-13 13:43:43 +0300

    feat: add class graph, vetrices and edges and functions to interact with them feat: add module getAllCycles and getLenCycle

---
+++ src/main.ts
@@ -0,0 +1,255 @@
+class Vertex{
+    id:number
+    name:string
+    edges:Map<string, Edge> // id, edge
+    indegree:number
+    outdegree:number
+    visited:boolean
+    constructor(id:number,name:string) {
+        this.id = id
+        this.name = name || `узел #${id}`
+        this.edges = new Map()
+        this.indegree = 0
+        this.outdegree = 0
+        this.visited = false
+    }
+
+    updDegrees():void{
+        this.indegree = (h.edges.filter(x => x[0][1]==this.id.toString()).length)
+        this.outdegree = (h.edges.filter(x => x[0][0]==this.id.toString()).length)
+    }
+
+    rename(newName:string):void{
+        let oldName = this.name
+        this.name = newName
+        console.log(`Edit: vertex "${oldName}" -> "${this.name}"`)
+    }
+}
+class Edge {
+    id:string
+    start:Vertex
+    end:Vertex
+    weight:number
+    constructor(start:number,end:number,weight:number) {
+        this.start = h.vertices.get(start)
+        this.end = h.vertices.get(end)
+        this.weight = weight
+        this.id = `${start}${end}`
+    }
+    reweigh(newW:number){
+        let oldW = this.weight
+        this.weight = newW
+        console.log(`Edit: edge "${oldW}" -> "${this.weight}"`)
+    }
+}
+
+class Graph {
+    #vertices = new Map<number,Vertex>(); // id,Vertex>
+    #edges = new Map<string, Edge>();
+    #adjacentList = new Map<number,number[]>(); // vertex id, edges ids
+
+    getRootVert():Vertex{
+        if (this.vertices.size>0) {
+            let [first] = this.vertices.values()
+            return first
+        }
+        throw new Error("Err: no vertices in graph");
+    }
+
+    get vertices() {
+        return this.#vertices
+    }
+
+    get edges() {
+        return Array.from(this.#edges)
+    }
+
+    get adjacentList() {
+        const list:any = {};
+        this.#adjacentList.forEach((val, key) => {
+            list[key] = Array.from(val)
+        })
+        return list
+    }
+
+    addVert(id:number, name:string="") {
+        if(!this.vertExist(id)) {
+            let vertex = new Vertex(id,name)
+            this.vertices.set(id, vertex)
+            this.#adjacentList.set(vertex.id, []);
+        }
+        else throw new Error(`Err: the vertex #${id} is already exist`);
+    }
+
+    editVert(id:number, newName:string){
+        if(this.vertExist(id)) {
+            this.vertices.get(id).rename(newName)
+        }
+        else throw new Error(`Err: the vertex #${id} is not exist`);
+    }
+
+    delVert(id:number) {
+        if(this.vertExist(id)) {
+            Array.from(this.vertices.get(id).edges).forEach(([id,edge], number)=>{
+                this.#edges.delete(id)
+            })
+            this.#adjacentList.delete(id)
+            this.vertices.delete(id)
+            console.log(`Del: vertex #${id}`);
+        } else throw new Error(`Err: the vertex #${id} is not exist`);
+    }
+
+    editEdge(start:number, end:number,newW:number) { // edit start end
+        if(this.edgeExist(`${start}${end}`)) {
+            this.#edges.get(`${start}${end}`).reweigh(newW)
+        } else throw new Error(`Err: the edge ${start}->${end} is not exist`);
+    }
+
+    delEdge(start:number, end:number) { // edit start end
+        if(this.edgeExist(`${start}${end}`)) {
+            if (this.vertExist(start)) this.vertices.get(start).edges.delete(`${start}${end}`)
+            let edgeArr = this.#adjacentList.get(start).filter(x=>x!==end)
+            this.#adjacentList.set(start, edgeArr)
+            console.log(`Del: edge ${start}->${end}`);
+        } else throw new Error(`Err: the edge ${start}->${end} is not exist`);
+    }
+
+    addEdge(start:number, end:number, weight:number) {
+        if(!this.edgeExist(`${start}${end}`) && this.vertices.has(start) && this.vertices.has(end) ) {
+            let edge = new Edge(start, end, weight)
+            this.#edges.set(edge.id,edge)
+            this.vertices.get(start).edges.set(edge.id, edge)
+            this.#adjacentList.get(start).push(end)
+            this.vertices.get(start).updDegrees()
+            this.vertices.get(end).updDegrees()
+
+        } else throw new Error(`Err: the edge ${start}->${end} is already exist or no such vertices exist`);
+    }
+
+    printStringify():void {
+        let str = '';
+        this.#adjacentList.forEach((val, key) => {
+            str += `${key} => ${Array.from(val.sort()).join(', ') || '/'};\n`;
+        });
+        console.log(str);
+    }
+
+    vertExist(id:number):boolean{
+        return this.vertices.has(id)
+    }
+
+    edgeExist(id:string):boolean{
+        return this.#edges.has(id)
+    }
+
+    getFirst(id:number):number | undefined {
+        if(this.vertExist(id)) {
+            if(this.vertices.get(id).edges.size > 0) {
+                let [first] = this.vertices.get(id).edges.values()
+                return (first.end.id)
+            }
+            return this.getRootVert().id
+        }
+        else {
+            throw new Error(`Err: the vertex #${id} is not exist`);
+            return undefined
+        }
+    }
+
+    getNext(id:number, i:number):number {
+        if(this.vertExist(id)) {
+            let edgeSize = this.vertices.get(id).edges.size
+            if(edgeSize > 0 && i< edgeSize) {
+                return +Array.from(this.vertices.get(id).edges)[i][0][1]
+            }
+            return this.getRootVert().id
+        }
+        else {
+            throw new Error(`Err: the vertex #${id} is not exist`);
+        }
+    }
+
+    getVertex(id:number):Vertex {
+        if (this.vertExist(id)) {
+            return this.vertices.get(id)
+        }
+        throw new Error(`Err: the vertex #${id} is not exist`);
+    }
+
+}
+
+function getAllCycles(graph:Graph):object {
+    const {vertices, adjacentList} = graph;
+    let res: number[][] = []
+    let graphCycles:object = {}
+    function recursiveVisit(currId:number, path:number[]) {
+        if (path.includes(currId)) {
+            path.push(currId)
+            res.push(path);
+            return
+        }
+        if (Number.isInteger(currId)) path.push(currId)
+        if ((adjacentList.hasOwnProperty(currId) && adjacentList[currId].length != 0) && path[0]!=currId) {
+            for (const neighbor of adjacentList[currId]) {
+                recursiveVisit(neighbor,[...path])
+            }
+        }
+        return path
+    }
+    for (const [vertex, neighbors] of Object.entries(adjacentList)) {
+        neighbors.forEach((nextId:string)=>{
+            recursiveVisit(Number(nextId), [Number(vertex)])
+            res.forEach((currentArr:number[]) => {
+                if (currentArr[0]==currentArr.at(-1)) {
+                    let arr = getFormat(currentArr)
+                    if (isUniq(arr)) {
+                        if (graphCycles.hasOwnProperty(arr.length))
+                            {
+                                graphCycles[arr.length].push(currentArr)
+                            }
+                        else
+                            {
+                                graphCycles[arr.length] = [currentArr]
+                            }
+                    }
+                }
+            })
+            res=[]
+        })
+    }
+    function isUniq(arr:number[]):boolean {
+        if (!graphCycles.hasOwnProperty(arr.length)) return true
+        let uniq = true
+        graphCycles[arr.length].forEach((currArr:number[])=>{
+            if (getFormat(currArr).join() == arr.join()) uniq = false
+        })
+        return uniq
+    }
+    function getFormat(arr:number[]):number[] {
+        return Array.from(new Set(arr)).sort();
+    }
+    return graphCycles;
+}
+function getLenCycle(graph:Graph,len:number) {
+    return getAllCycles(graph)[len] || `циклов длины ${len} нет`
+}
+const h = new Graph();
+
+h.addVert(1)
+h.addVert(2,"Example")
+h.addVert(3)
+h.addVert(4)
+h.addVert(5)
+
+h.addEdge(1,2,1)
+h.addEdge(2,3,3)
+h.addEdge(3,4,1)
+h.addEdge(3,5,2)
+h.addEdge(4,2,6)
+h.addEdge(4,1,8)
+
+h.printStringify()
+console.log(getLenCycle(h,3))
+
+
+
